64ec694003a6f087f2ff89f11216270c6a24613c
lopez <patrice.lopez@science-miner.com>
2019-09-18 22:14:21 +0200

    add all training command options

--- src/main/java/org/grobid/trainer/DataseerTrainerRunner.java
+++ src/main/java/org/grobid/trainer/DataseerTrainerRunner.java
@@ -19,7 +19,7 @@ public class DataseerTrainerRunner {
             + "-t NBThreads";
 
     enum RunType {
-        TRAIN, EVAL, SPLIT;
+        TRAIN, EVAL, SPLIT, EVAL_N_FOLD;
 
         public static RunType getRunType(int i) {
             for (RunType t : values()) {
@@ -81,6 +81,7 @@ public class DataseerTrainerRunner {
         double epsilon = 0.000001;
         int window = 20;
         int nbMaxIterations = 0;
+        int numFolds = 10;
 
         for (int i = 0; i < args.length; i++) {
             if (i == 4) {
@@ -91,6 +92,15 @@ public class DataseerTrainerRunner {
                     split = Double.parseDouble(args[i]);
                 } catch (Exception e) {
                     throw new IllegalStateException("Invalid split value: " + args[i]);
+                }
+            } else if (args[i].equals("-n")) {
+                if (i + 1 == args.length) {
+                    throw new IllegalStateException("Missing number of folds value. ");
+                }
+                try {
+                    numFolds = Integer.parseInt(args[i + 1]);
+                } catch (Exception e) {
+                    throw new IllegalStateException("Invalid number of folds value: " + args[i + 1]);
                 }
             } /*else if (args[i].equals("-b")) {
                 if ((mode == RunType.TRAIN) && (args.length >= 7)) {
@@ -122,6 +132,12 @@ public class DataseerTrainerRunner {
             case SPLIT:
                 System.out.println(AbstractTrainer.runSplitTrainingEvaluation(trainer, split));
                 break;
+            case EVAL_N_FOLD:
+                if(numFolds == 0) {
+                    throw new IllegalArgumentException("N should be > 0");
+                }
+                System.out.println(AbstractTrainer.runNFoldEvaluation(trainer, numFolds));
+                break;    
             default:
                 throw new IllegalStateException("Invalid RunType: " + mode.name());
         }
